c824cf7924f827ea119f4f681952ad8b90b527e1
Gilbert Zaini <gilbertzaini03@gmail.com>
2023-09-27 11:46:10 +0700

    Add files via upload

---
+++ backend/controllers/userController.js
@@ -0,0 +1,11 @@
+import { where } from "sequelize";
+// import User from "../models/UserModel.js";
+
+// export const getUsers = async (req, res) => {
+//   try {
+//     const response = await User.findAll();
+//     res.status(200).json(response);
+//   } catch (e) {
+//     console.log(e.message);
+//   }
+// };
