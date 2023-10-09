e7714bbf6b8baff96aab09a349431c8365dfc6a2
Gilbert Zaini <gilbertzaini03@gmail.com>
2023-10-09 13:04:32 +0700

    wrapped app in chakraprovider

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,18 @@
-import './App.css';
-import Test from './Components/Test';
-import * as React from 'react'
-import { ChakraProvider } from '@chakra-ui/react'
-
-
-
+import "./App.css";
+import Test from "./Components/Test";
+import * as React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
-  return(
-    <div className="App">
-      <header className="App-header">
-      <Test />
-      </header>
-    </div>
-  )
+  return (
+    <ChakraProvider>
+      {/* <div className="App"> */}
+        <div className="App-header">
+          <Test />
+        </div>
+      {/* </div> */}
+    </ChakraProvider>
+  );
 }
 
 export default App;
