ba1260f80d719444d68208c5691bf3d0293e3a9b
Gilbert Zaini <gilbertzaini03@gmail.com>
2023-10-21 00:13:44 +0700

    feat(be): new user apis

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,11 +3,38 @@ const { User } = require("../models");
 const getUsers = async (req, res) => {
   try {
     const response = await User.findAll();
-    res.status(200).json(response);
+    res.status(202).json(response);
     console.log("done");
   } catch (e) {
     console.log(e.message);
   }
 };
 
-module.exports = { getUsers };
+const updateUser = async(req, res) => {
+  try{
+      const response = await User.update(req.body, {
+          where: {
+              user_id: req.params.user_id,
+          }
+      });
+      res.status(203).json({msg: "User Updated"});
+  }catch(e){
+      console.log(e.message);
+  }
+}
+
+const deleteUser = async (req, res) => {
+  try {
+    await User.destroy({
+      where: {
+        user_id: req.params.user_id,
+      },
+    });
+    res.status(204).json({ msg: "User Deleted" });
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+
+
+module.exports = { getUsers, updateUser, deleteUser };
--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,12 +1,10 @@
 const express = require("express");
-const {getUsers} = require("../controllers/userController.js");
+const {getUsers, updateUser, deleteUser} = require("../controllers/userController.js");
 
 const userRouter = express.Router();
 
 userRouter.get('/users', getUsers);
-// userRouter.get('/users/:id', getUserById);
-// userRouter.post('/users', createUser);
-// userRouter.patch('/users/:id', updateUser);
-// userRouter.delete('/users/:id', deleteUser);
+userRouter.patch('/users/:id', updateUser);
+userRouter.delete('/users/:id', deleteUser);
 
 module.exports = userRouter;
