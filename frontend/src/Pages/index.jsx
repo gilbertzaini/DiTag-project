1ceaab61f5ee3ffc3d679fc22a760b5a2e745a19
Gilbert Zaini <gilbertzaini03@gmail.com>
2023-11-07 22:24:59 +0700

    fix(fe): index now can get logged in user's data

--- frontend/src/Pages/index.jsx
+++ frontend/src/Pages/index.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import Navbar from '../Components/Navbar'
 import HomePage from '../Components/HomePage'
 import About from '../Components/About'
+import { useDispatch } from "react-redux";
+import { getMe } from "../Features/authSlice";
 
 const Index = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getMe());
+  }, [dispatch]);
+
   return (
     <>
         <Navbar/>
