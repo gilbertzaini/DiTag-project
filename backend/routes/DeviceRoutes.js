c0685d404268aac6ed517c21d39f0e9dfe03b218
Gilbert Zaini <gilbertzaini03@gmail.com>
2023-12-19 22:35:22 +0700

    update lupa apa aja

--- backend/routes/DeviceRoutes.js
+++ backend/routes/DeviceRoutes.js
@@ -6,7 +6,7 @@ const deviceRouter = express.Router();
 
 deviceRouter.get('/device/:user_id', verifyUser, getDevice);
 deviceRouter.post('/device/register', verifyUser, registerDevice);
-deviceRouter.patch('/device/update/:device_id', verifyUser, updateDevice);
-deviceRouter.delete('/device/delete/:device_id', verifyUser, deleteDevice);
+deviceRouter.patch('/device/:device_id', verifyUser, updateDevice);
+deviceRouter.delete('/device/:device_id', verifyUser, deleteDevice);
 
 module.exports = deviceRouter;
